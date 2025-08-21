eead9336b904afc8909b5045ae561b27b8b4837b
Matias <martinezmatiemi@gmail.com>
2025-08-20 23:39:54 -0300

    .

--- src/pages/EventDetails.tsx
+++ src/pages/EventDetails.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { Spinner } from "@chakra-ui/react";
+import { Spinner, VStack, Text, Container, Center } from "@chakra-ui/react";
 
 import { useGetEvent } from "../hooks/useGetEvent";
 import EventDetail from "../components/EventDetail";
@@ -9,11 +9,24 @@ export default function EventDetails() {
   const { id } = useParams();
   const { event, isLoading: isEventLoading } = useGetEvent(id);
 
-  if (isEventLoading) return <Spinner />;
+  if (isEventLoading) {
+    return (
+      <Container maxW="container.xl" h="100vh">
+        <Center h="70vh">
+          <VStack spacing={6}>
+            <Spinner thickness="4px" speed="0.65s" emptyColor="gray.600" color="brand.500" size="xl" />
+            <Text color="whiteAlpha.700" fontSize="md" textAlign="center">
+              Buscando evento...
+            </Text>
+          </VStack>
+        </Center>
+      </Container>
+    );
+  }
 
   if (!event) {
     navigate("/");
-    return;
+    return null;
   }
 
   return <EventDetail {...event} />;
