4afafc69340f226e2f72e3c8c2ffff7ff77f674e
Matias <martinezmatiemi@gmail.com>
2025-09-20 09:07:17 -0300

    :

--- src/pages/UserTickets.tsx
+++ src/pages/UserTickets.tsx
@@ -30,8 +30,8 @@ export default function UserTickets() {
   const { tickets, isLoading, refreshTickets, selectedTicket, isOpen, onClose, handleShowQR, qrToken, timeLeft, generatingTicketId } = useUserTickets();
   const eventInfoModal = useEventInfoModal();
 
-  const activeEvents: string[] = [];
-  const ongoingEvents: string[] = ["primavera-en-el-bosque-2025"];
+  const activeEvents: string[] = ["primavera-en-el-bosque-2025"];
+  const ongoingEvents: string[] = [];
 
   return (
     <Box py={[0, 20]}>
